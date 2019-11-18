caad0d646a78c0411565d3e244420ea615272d00
Vinicius Resende dos Santos <vini_handball@hotmail.com>
2019-11-18 18:19:50 -0300

    Update db_config.php

--- example/db_config.php
+++ example/db_config.php
@@ -14,21 +14,6 @@
             ]
         ]);
 
-    define("DATA_LAYER_CONFIG",
-        [
-            "host" => "186.202.41.211",
-            "port" => "1433",
-            "dbname" => "acaocompras1",
-            "username" => "sql_admin",
-            "passwd" => "TGAAGT@loc2018@",
-            "options" => [
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-                PDO::ATTR_CASE => PDO::CASE_NATURAL
-            ]
-        ]);
-
 
 
 
